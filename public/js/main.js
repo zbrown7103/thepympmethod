0d5d4d26966737451c922ba47bf0a0f557b86791
zbrown001 <89473642+zbrown001@users.noreply.github.com>
2023-04-14 16:13:17 -0600

    run

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = 'https://ythsyfvsbxedhfzeczsr.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0aHN5ZnZzYnhlZGhmemVjenNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE1MDEwNjYsImV4cCI6MTk5NzA3NzA2Nn0.l49PRyS8Xuvp-ZJEkU33c86-sQ6QNpSSGaMmjAhWKkE';
 
--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,6 @@
+const { supabase } = require('../supabase');
 const express = require('express');
 const router = express.Router();
-const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = 'https://ythsyfvsbxedhfzeczsr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0aHN5ZnZzYnhlZGhmemVjenNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE1MDEwNjYsImV4cCI6MTk5NzA3NzA2Nn0.l49PRyS8Xuvp-ZJEkU33c86-sQ6QNpSSGaMmjAhWKkE';
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Get all items
 router.get('/', async (req, res) => {
